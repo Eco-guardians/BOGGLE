d8cb265ed5378c0f28efc30dab1322d1cb6e8616
Mineru <68109760+mineruking@users.noreply.github.com>
2024-08-08 23:30:13 +0900

    UI 1차 수정

--- lib/myhome.dart
+++ lib/myhome.dart
@@ -103,6 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     return DefaultTabController(
+      // DefaultTabController로 Scaffold 전체를 감쌈
       length: 2, // 상단 탭의 수
       child: Scaffold(
         appBar: AppBar(
@@ -118,23 +119,12 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
           centerTitle: false,
-          bottom: TabBar(
-            tabAlignment: TabAlignment.start,
-            isScrollable: true, // 탭이 스크롤 가능하게 설정
-            tabs: [
-              Tab(text: '어항'),
-              Tab(text: '포인트'),
-            ],
-            indicatorColor: Color.fromARGB(255, 196, 42, 250), // 인디케이터 색상
-            labelStyle: TextStyle(
-              fontWeight: FontWeight.bold,
-            ), // 탭의 글씨를 굵게 설정
-            labelColor: Color.fromARGB(255, 196, 42, 250), // 선택된 탭의 색상
-            unselectedLabelColor: Colors.black, // 선택되지 않은 탭의 색상
-            indicator: UnderlineTabIndicator(
-              borderSide: BorderSide(
-                width: 2.0,
-                color: Color.fromARGB(255, 196, 42, 250),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(50),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
               ),
             ),
           ),
@@ -177,124 +167,371 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            height: 200,
-            child: Stack(
+          _buildHeaderSection(),
+          SizedBox(height: 20),
+          _buildUserTreasuresSection(),
+          SizedBox(height: 20),
+          _buildWaterInformationSection(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeaderSection() {
+    // 포인트에 따른 이미지와 텍스트를 정의
+    String imagePath;
+    String message;
+
+    if (_points <= 50) {
+      imagePath = 'image/Whale.png';
+      message = _nickname + '님의 활동이 보글과 환경 오염을 지키고 있어요!\n계속 이대로 가주세요!';
+    } else if (_points <= 100) {
+      imagePath = 'image/fish.png';
+      message = _nickname + '님의 작은 움직임으로\n보글과 환경 오염을 지킬 수 있어요!\n한 걸음씩 걸어가볼까요?';
+    } else {
+      imagePath = 'image/tortoise.png';
+      message = _nickname + '님, 느려도 괜찮아요!\n흐르가는대로 천천히 해주세요\n당신의 한걸음이 지구를 바꿔요!';
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        // Center 위젯으로 감싸서 중앙에 배치
+        child: Column(
+          mainAxisSize: MainAxisSize.min, // 최소 크기를 설정하여 자식이 있는 만큼만 공간 차지
+          children: [
+            Image.asset(
+              imagePath,
+              height: 100,
+            ),
+            SizedBox(height: 20),
+            Text(
+              message,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserTreasuresSection() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            _nickname + '님의 보글을 꾸며봐요!',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          GridView.count(
+            crossAxisCount: 2,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            children: [
+              _buildCard('내 보글', 'image/wave.png', '', '', 61, 168, 224),
+              _buildCard('내 포인트', 'image/gacha.png', _points.toString(), '/150',
+                  207, 154, 225)
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWaterInformationSection() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildlongCard('활동하기', 'image/soap.png', 'image/gutter.png',
+              'image/Quiz.png', '0/1', '', 192, 155, 255),
+          Text(
+            ' ',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '우리 지역의 물 관련 정보를 알아봐요!',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          _buildWaterQualityCard(),
+          SizedBox(height: 10),
+          _buildWaterTravelCard(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(String title, String imagePath, String point,
+      String additionalInfo, int r, int g, int b) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(240, r, g, b),
+        borderRadius: BorderRadius.circular(35),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0), // 패딩 추가
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // 텍스트 좌측 정렬
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+            Row(
+              // Row 위젯으로 이미지 가로 정렬
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceEvenly, // 이미지 사이 공간 균일하게 배치
               children: [
-                if (_points <= 10)
-                  Image.asset(
-                    'image/Poor.png',
-                    fit: BoxFit.contain,
-                    width: double.infinity,
-                    height: double.infinity,
-                  )
-                else if (_points > 10 && _points <= 25)
-                  Image.asset(
-                    'image/FishBowl.png',
-                    fit: BoxFit.contain,
-                    width: double.infinity,
-                    height: double.infinity,
-                  )
-                else ...[
-                  Image.asset(
-                    'image/background.png',
-                    fit: BoxFit.contain,
-                    width: double.infinity,
-                    height: double.infinity,
-                  ),
-                  if (_points > 25)
-                    Image.asset(
-                      'image/bubble.png',
-                      fit: BoxFit.contain,
-                      width: double.infinity,
-                      height: double.infinity,
-                    ),
-                  if (_points > 50)
-                    Image.asset(
-                      'image/fish_far.png',
-                      fit: BoxFit.contain,
-                      width: double.infinity,
-                      height: double.infinity,
-                    ),
-                  if (_points > 75)
-                    Image.asset(
-                      'image/weeds.png',
-                      fit: BoxFit.contain,
-                      width: double.infinity,
-                      height: double.infinity,
-                    ),
-                  if (_points > 100)
-                    Image.asset(
-                      'image/fishes.png',
-                      fit: BoxFit.contain,
-                      width: double.infinity,
-                      height: double.infinity,
-                    ),
-                ],
+                Image.asset(imagePath, height: 50),
               ],
             ),
+            SizedBox(height: 10),
+            SizedBox(height: 5),
+            Text(
+              point,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              additionalInfo,
+              style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWaterQualityCard() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 251, 240, 255),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 2),
           ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('이달의 수질',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              Text(_location ?? 'Unknown'),
+            ],
+          ),
+          SizedBox(height: 10),
+          Text(
+            '${_waterQuality.toString()} ppm',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          _buildWaterQualityBar(),
+          SizedBox(height: 10),
+          _buildWaterQualityIndicators(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildlongCard(
+    String title,
+    String imagePath1,
+    String imagePath2,
+    String imagePath3,
+    String point,
+    String additionalInfo,
+    int r,
+    int g,
+    int b,
+  ) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(240, r, g, b),
+        borderRadius: BorderRadius.circular(35),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0), // 패딩 추가
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // 텍스트 좌측 정렬
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+            Row(
+              // Row 위젯으로 이미지 가로 정렬
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceEvenly, // 이미지 사이 공간 균일하게 배치
+              children: [
+                Image.asset(imagePath1, height: 50),
+                Image.asset(imagePath2, height: 50),
+                Image.asset(imagePath3, height: 50),
+              ],
+            ),
+            SizedBox(height: 10),
+            SizedBox(height: 5),
+            Text(
+              point,
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              additionalInfo,
+              style: TextStyle(fontSize: 14),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWaterQualityBar() {
+    return Column(
+      children: [
+        Row(
+          children: List.generate(10, (index) {
+            return Expanded(
+              child: Container(
+                height: 8,
+                color: _getColorForIndex(index),
+              ),
+            );
+          }),
+        ),
+        Stack(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(11, (index) => Text('$index')),
+            ),
+            Positioned(
+              left: _getArrowPosition(),
+              child: Icon(
+                Icons.arrow_drop_up,
+                color: _getColorForArrow(),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildWaterQualityIndicators() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildWaterQualityRow(Icons.water_drop, Colors.blue, '매우 좋음',
+              '간단한 정수 후 마실 수 있음', 0.0, 1.0),
+          SizedBox(height: 10),
+          _buildWaterQualityRow(Icons.water_drop, Colors.lightBlue, '좋음',
+              '일반 정수 처리 후 마실 수 있음', 1.0, 2.0),
+          SizedBox(height: 10),
+          _buildWaterQualityRow(Icons.water_drop, Colors.green, '약간 좋음',
+              '일반 정수 처리 후 마실 수 있음', 2.0, 3.0),
+          SizedBox(height: 10),
+          _buildWaterQualityRow(Icons.water_drop, Colors.blueGrey, '보통',
+              '일반 정수 후 공업용수로 사용 가능', 3.0, 6.0),
+          SizedBox(height: 10),
+          _buildWaterQualityRow(
+              Icons.water_drop,
+              const Color.fromARGB(255, 172, 155, 1),
+              '약간 나쁨',
+              '농업용수로 사용 가능',
+              6.0,
+              8.0),
+          SizedBox(height: 10),
+          _buildWaterQualityRow(Icons.water_drop, Colors.orange, '나쁨',
+              '특수처리 후 공업용수로 사용 가능', 8.0, 10.0),
+          SizedBox(height: 10),
+          _buildWaterQualityRow(
+              Icons.water_drop, Colors.red, '매우 나쁨', '이용 불가능', 10.0, 100),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWaterTravelCard() {
+    return Container(
+      height: 200,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('image/water_travel.png'),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('이달의 수질', style: indicatorTextStyle),
-                Text(_location ?? 'Unknown'),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
                 Text(
-                  '${_waterQuality.toString()} ppm',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: List.generate(10, (index) {
-                    return Expanded(
-                      child: Container(
-                        height: 8,
-                        color: _getColorForIndex(index),
-                      ),
-                    );
-                  }),
-                ),
-                Stack(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('0'),
-                        Text('1'),
-                        Text('2'),
-                        Text('3'),
-                        Text('4'),
-                        Text('5'),
-                        Text('6'),
-                        Text('7'),
-                        Text('8'),
-                        Text('9'),
-                        Text('10'),
-                      ],
-                    ),
-                    Positioned(
-                      left: _getArrowPosition(),
-                      child: Icon(
-                        Icons.arrow_drop_up,
-                        color: _getColorForArrow(), // 화살표 색상 적용
-                      ),
-                    ),
-                  ],
-                ),
+                  '내 주변 물 여행지',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                )
               ],
             ),
           ),
-          _buildWaterQualityInfo(),
         ],
       ),
     );
@@ -476,47 +713,6 @@ class _MyHomePageState extends State<MyHomePage> {
         SizedBox(height: 10),
         Text(label),
       ],
-    );
-  }
-
-  Widget _buildWaterQualityInfo() {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '수질 등급',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          _buildWaterQualityRow(Icons.water_drop, Colors.blue, '매우 좋음',
-              '간단한 정수 후 마실 수 있음', 0.0, 1.0),
-          SizedBox(height: 10),
-          _buildWaterQualityRow(Icons.water_drop, Colors.lightBlue, '좋음',
-              '일반 정수 처리 후 마실 수 있음', 1.0, 2.0),
-          SizedBox(height: 10),
-          _buildWaterQualityRow(Icons.water_drop, Colors.green, '약간 좋음',
-              '일반 정수 처리 후 마실 수 있음', 2.0, 3.0),
-          SizedBox(height: 10),
-          _buildWaterQualityRow(Icons.water_drop, Colors.blueGrey, '보통',
-              '일반 정수 후 공업용수로 사용 가능', 3.0, 6.0),
-          SizedBox(height: 10),
-          _buildWaterQualityRow(
-              Icons.water_drop,
-              const Color.fromARGB(255, 172, 155, 1),
-              '약간 나쁨',
-              '농업용수로 사용 가능',
-              6.0,
-              8.0),
-          SizedBox(height: 10),
-          _buildWaterQualityRow(Icons.water_drop, Colors.orange, '나쁨',
-              '특수처리 후 공업용수로 사용 가능', 8.0, 10.0),
-          SizedBox(height: 10),
-          _buildWaterQualityRow(
-              Icons.water_drop, Colors.red, '매우 나쁨', '이용 불가능', 10.0, 100),
-        ],
-      ),
     );
   }
 
